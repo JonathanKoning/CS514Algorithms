9b03641b2dd2915cf17d1c5d00bc4a1e2163cdf0
JonathanKoning <jskoning99@gmail.com>
2021-10-04 12:24:52 -0700

    hw2.py accepts options for collecting data. Data files for random values, ordered arrays, and reverse ordered arrays

--- assignment2/hw2.py
+++ assignment2/hw2.py
@@ -3,9 +3,30 @@ import getopt
 import math
 import datetime
 import random
+import csv
+import time
+
+def randomData(num):
+	array = []
+	# for i in range(num):
+	# 	array.append(random.randint(0, 1000000))
+	i = 0
+	while i < num:
+		array.append(i)
+		i+=1
+
+	return array
+
 
 def getinput():
 	array = []
+
+	if(len(sys.argv) == 1):
+		while(len(array) < 1):
+			arr = input("please enter a list of numbers: ")
+			array = list(map(int, arr.split(' ')))
+
+		return array
 
 	if(sys.argv[1] == "-r"):
 		num = ""
@@ -22,11 +43,14 @@ def getinput():
 			except:
 				pass
 		
-		for i in range(num):
-			array.append(random.randint(0, 1000000))
+		# for i in range(num):
+			# array.append(random.randint(0, 1000000))
 		
-		return array
+		return randomData(num)
 
+	elif(sys.argv[1] == "-d"):
+		dataMode()
+		return "datamode"
 
 	try:
 		array = list(map(int, sys.argv[1:]))
@@ -87,9 +111,47 @@ def quickSort(A):
 		right = [x for x in A[1:] if x >= pivot]
 		return quickSort(left) + [pivot] + quickSort(right)
 
+
+def dataMode():
+	points = 1000
+	# points = [1, 10, 50, 100, 500, 1000, 1500, 2000, 2500, 5000, 10000, 15000]
+	with open("results_1000s.csv", 'w', newline='') as f:
+		row = ["n", "mergeSort", "quickSort"]
+		writer = csv.writer(f)
+		writer.writerow(row)
+
+
+	for i in range(1,points):
+	# for i,x in enumerate(points):
+		# print(i)
+		A = randomData(i)
+		# print(A)
+		# start = datetime.datetime.now()
+		start = time.perf_counter()
+		sorted = mergeSort(A)
+		# stop = datetime.datetime.now()
+		stop = time.perf_counter()
+		mergeSortduration = stop-start
+	
+		# start = datetime.datetime.now()
+		start = time.perf_counter()
+		sorted = quickSort(A)
+		# stop = datetime.datetime.now()
+		stop = time.perf_counter()
+		quickSotduration = stop-start
+		# print("quickSort: ", sorted)
+		row = [str(i), str(mergeSortduration), str(quickSotduration)]
+		with open("results_1000s.csv", 'a', newline='') as f:
+			writer = csv.writer(f)
+			writer.writerow(row)
+
+
+
 if __name__ == "__main__":
 	A = getinput()
 	# print(A)
+	if(A == "datamode"):
+		exit()
 
 	start = datetime.datetime.now()
 	sorted = mergeSort(A)
