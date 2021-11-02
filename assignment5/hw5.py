e2fe79fe07025bcba46546e353b86e68bdd4f8d6
JonathanKoning <jskoning99@gmail.com>
2021-11-02 14:50:53 -0700

    number of check operations brought down by 4 per cycle. PDF report complete

--- assignment5/hw5.py
+++ assignment5/hw5.py
@@ -16,6 +16,8 @@
 # Both initial and goal state is a 3x3 matrix represented by a Python list, from left to right, and from top to bottom, where the empty cell is represented by 0. For example, the start and goal states above would be represented as [1,2,3,4,5,6,8,7,0] and [1,2,3,8,0,4,7,6,5] respectively.
 # There might be multiple solutions (i.e. shortest path) for each problem, thus we ask you to return the length of the shortest path.
 
+# To run:
+# python -c 'from hw5 import ShortestPath; print( ShortestPath([1,2,3,8,0,4,7,6,5], [[1,2,3,4,5,6,8,7,0], [8, 3, 4, 0, 7, 1, 6, 2, 5]]))'
 
 def ShortestPath(goal, states):
 	# print("ShortestPath")
@@ -32,32 +34,35 @@ def ShortestPath(goal, states):
 	while len(Q) != 0 and i != len(states):
 		u = Q.pop(0)
 		stru = str(u)
+		level = dist[stru] + 1
 		#find all neighbors of u
 		#up, down, left, right
 		empty = u.index(0)
 		#check if empty can move up
-		if(empty != 0 and empty != 1 and empty != 2):
+		# if(empty != 0 and empty != 1 and empty != 2):
+		if(empty > 2):
 			# print("Move empty up")
 			check = u[:]
 			check[empty], check[empty-3] = check[empty-3], check[empty]
 			strcheck = str(check)
 			# if str(check) not in dist:
 			if strcheck not in dist:
-				dist[strcheck] = dist[stru] + 1
+				dist[strcheck] = level
 				if strcheck in inits:
-					inits[strcheck] = dist[stru] + 1
+					inits[strcheck] = level
 					i+=1
 				Q.append(check)
 		#check if empty can move down
-		if(empty != 6 and empty != 7 and empty != 8):
+		# if(empty != 6 and empty != 7 and empty != 8):
+		if(empty < 6):
 			check = u[:]
 			check[empty], check[empty+3] = check[empty+3], check[empty]
 			strcheck = str(check)
 			# if str(check) not in dist:
 			if str(check) not in dist:
-				dist[strcheck] = dist[stru] + 1
+				dist[strcheck] = level
 				if strcheck in inits:
-					inits[strcheck] = dist[stru] + 1
+					inits[strcheck] = level
 					i+=1
 				Q.append(check)
 		#check if empty can move left
@@ -66,9 +71,9 @@ def ShortestPath(goal, states):
 			check[empty], check[empty-1] = check[empty-1], check[empty]
 			strcheck = str(check)
 			if strcheck not in dist:
-				dist[strcheck] = dist[stru] + 1
+				dist[strcheck] = level
 				if strcheck in inits:
-					inits[strcheck] = dist[stru] + 1
+					inits[strcheck] = level
 					i+=1
 				Q.append(check)
 		#Check if empty can move right
@@ -77,9 +82,9 @@ def ShortestPath(goal, states):
 			check[empty], check[empty+1] = check[empty+1], check[empty]
 			strcheck = str(check)
 			if strcheck not in dist:
-				dist[strcheck] = dist[stru] + 1
+				dist[strcheck] = level
 				if strcheck in inits:
-					inits[strcheck] = dist[stru] + 1
+					inits[strcheck] = level
 					i+=1
 				Q.append(check)
 
